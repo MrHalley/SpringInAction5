806bbe8804f935e0b11de8bd2d48c19d61c9d727
Mr.Du <1165449791@qq.com>
2020-09-16 15:34:31 +0800

    创建Spring初始化项目

---
+++ ch1/taco-clouds/src/main/java/tacos/TacoCloudsApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudsApplication.class, args);
+    }
+
+}
